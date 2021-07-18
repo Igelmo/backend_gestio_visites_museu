651f6a9657828078db4c29dbe1c234284bd88e23
Laia <laia.igelmo.a@gmail.com>
2021-07-18 14:23:35 +0200

    Add call to endpoint. Add datamodel package.

--- src/main/kotlin/fib/museu/Application.kt
+++ src/main/kotlin/fib/museu/Application.kt
@@ -4,11 +4,16 @@ import fib.museu.plugins.MySQLDatabaseExampleKotlin
 import fib.museu.plugins.configureMonitoring
 import fib.museu.plugins.configureRouting
 import fib.museu.plugins.configureSerialization
+import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        install(CORS) {
+            anyHost()
+        }
         configureRouting()
         configureMonitoring()
         configureSerialization()
