e567394ce7ca400ac3c1a5e7f7616c478bf60336
Laia <laia.igelmo.a@gmail.com>
2021-07-25 20:20:14 +0200

    Add AssistantsType.kt enum class. Add Booking data structure. Get info from frontendpost.

---
+++ src/main/kotlin/fib/museu/datamodels/AssistantsType.kt
@@ -0,0 +1,15 @@
+package fib.museu.datamodels
+
+enum class AssistantsType {
+    Batxillerat,
+    CicleMitja,
+    ESO,
+    CicleSuperior,
+    Primaria,
+    Universitat,
+    FIB,
+    Associacio,
+    Extranger,
+    Particular,
+    Altre
+}
--- src/main/kotlin/fib/museu/datamodels/Booking.kt
+++ src/main/kotlin/fib/museu/datamodels/Booking.kt
@@ -1,2 +1,15 @@
 package fib.museu.datamodels
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Booking(
+    val name: String,
+    val surname: String,
+    val center: String,
+    val email: String,
+    val phone: String,
+    val assistants: Int,
+    val assistantsType: AssistantsType,
+    val comments: String?
+)
