0d95a3d98ea631ae3b9458076aa7bd69be87d92b
laia <laia.igelmo.a@gmail.com>
2021-09-14 19:35:22 +0200

    change port to host

--- src/main/kotlin/fib/museu/Application.kt
+++ src/main/kotlin/fib/museu/Application.kt
@@ -10,7 +10,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
+    embeddedServer(Netty, host = "https://backend-museu.herokuapp.com/" ) {
         install(CORS) {
             allowNonSimpleContentTypes = true
             header(HttpHeaders.AccessControlAllowHeaders)
