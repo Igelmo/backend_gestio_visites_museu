afc2fa7928aa9aa99957b759699ff2df4ded96fc
laia <laia.igelmo.a@gmail.com>
2021-09-14 19:13:20 +0200

    change port to environment port

--- src/main/kotlin/fib/museu/Application.kt
+++ src/main/kotlin/fib/museu/Application.kt
@@ -10,7 +10,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080) {
+    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
         install(CORS) {
             allowNonSimpleContentTypes = true
             header(HttpHeaders.AccessControlAllowHeaders)
