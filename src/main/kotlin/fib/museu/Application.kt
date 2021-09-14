5e29253e68eaa008237b80acd66efb5a7552d554
laia <laia.igelmo.a@gmail.com>
2021-09-14 18:32:42 +0200

    AllowCredentials enabled on CORS

--- src/main/kotlin/fib/museu/Application.kt
+++ src/main/kotlin/fib/museu/Application.kt
@@ -10,9 +10,10 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080) {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         install(CORS) {
             allowNonSimpleContentTypes = true
+            allowCredentials = true
             anyHost()
             method(HttpMethod.Delete)
         }
